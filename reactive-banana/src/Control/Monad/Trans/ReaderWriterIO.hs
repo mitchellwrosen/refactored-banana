e156c2ffbb94254eafa59564941cecd5bbe68c58
Mitchell Rosen <mitchell@simspace.com>
2021-04-30 00:58:10 -0400

    deriving

--- reactive-banana/src/Control/Monad/Trans/ReaderWriterIO.hs
+++ reactive-banana/src/Control/Monad/Trans/ReaderWriterIO.hs
@@ -22,6 +22,7 @@ import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader (ReaderT (..))
 import Data.IORef
 import Data.Monoid
 import Data.Semigroup
@@ -30,23 +31,8 @@ import Data.Semigroup
     Type and class instances
 ------------------------------------------------------------------------------}
 newtype RWIO r w a = RWIO {run :: r -> IORef w -> IO a}
-
-instance Functor (RWIO r w) where
-  fmap f m = RWIO $ \x y -> fmap f (run m x y)
-
-instance Applicative (RWIO r w) where
-  pure a = RWIO $ \_ _ -> pure a
-  f <*> a = RWIO $ \x y -> run f x y <*> run a x y
-
-instance Monad (RWIO r w) where
-  return a = RWIO $ \_ _ -> return a
-  m >>= k = RWIO $ \x y -> run m x y >>= \a -> run (k a) x y
-
-instance MonadFix (RWIO r w) where
-  mfix f = RWIO $ \x y -> mfix (\a -> run (f a) x y)
-
-instance MonadIO (RWIO r w) where
-  liftIO m = RWIO \_ _ -> m
+  deriving stock (Functor)
+  deriving (Applicative, Monad, MonadFix, MonadIO) via (ReaderT r (ReaderT (IORef w) IO))
 
 instance (a ~ ()) => Semigroup (RWIO r w a) where
   mx <> my = mx >> my
